ff35c5d3ac22b0e7860235dc7a47270a9e8746e0
Hernando Calle Valencia <nandomhdc@gmail.com>
2021-07-29 16:42:24 -0400

    set active on click

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('hola bola')
+
+$(document).ready(() => {
+    var $navLinks = $('a.nav-link');
+    $navLinks.on('click', event => {
+        $navLinks.removeClass('active');
+        $(event.target).addClass('active');
+    });
+});
