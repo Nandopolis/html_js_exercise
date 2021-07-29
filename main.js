051b1bf1d76a27186c7044d42ecb9681744bd905
Hernando Calle Valencia <nandomhdc@gmail.com>
2021-07-29 16:51:14 -0400

    get type details

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 console.log('hola bola')
 
+const POKEAPI_URL = 'https://pokeapi.co/api/v2';
+
 $(document).ready(() => {
     var $navLinks = $('a.nav-link');
     $navLinks.on('click', event => {
+        var $navLink = $(event.target);
         $navLinks.removeClass('active');
-        $(event.target).addClass('active');
+        $navLink.addClass('active');
+
+        var type = $navLink.text().toLowerCase();
+        $.getJSON(`${POKEAPI_URL}/type/${type}/`, response => {
+            console.log('response', response);
+        })
     });
 });
